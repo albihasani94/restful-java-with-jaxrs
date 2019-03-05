46a1bdd945ff9ae6b819a5cc0719b7be3e955322
albihasani94 <albihasani94@gmail.com>
2019-03-05 23:46:46 +0100

    initial commit

---
+++ jaxrs/src/main/java/com/cert/jaxrs/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package com.cert.jaxrs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JAXRSConfiguration extends Application {
+
+}
